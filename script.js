2501a45c7bf44b5bd069cf867367aa98e5a49113
Deepanshu Tiwari <ddepu11@gmail.com>
2021-02-21 12:03:00 +0530

    Created reviews page and styled it

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const trendingURL =
 const hero = document.getElementById("hero");
 const form = document.querySelector("form");
 const query = document.querySelector("input");
+const reviewsDiv = document.querySelector(".reviews");
 const trending = document.getElementById("trending");
 let reviewBtn;
 
@@ -42,7 +43,7 @@ const showMoviesInDOM = (movies) => {
     hero.appendChild(div);
   });
   reviewBtn = document.querySelectorAll(".review-btn");
-  fetchReviews(reviewBtn);
+  fetchReviews(reviewBtn, movies);
 };
 
 // Fetching movies
@@ -73,25 +74,73 @@ trending.addEventListener("click", () => {
 });
 
 // Fetching Reviews by Movie ID
-function fetchReviews(reviewBtns) {
+function fetchReviews(reviewBtns, movies) {
   reviewBtns.forEach((btn) => {
     btn.addEventListener("click", async () => {
-      
       const id = btn.getAttribute("data-movieId");
       let reviewsURL = `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=1ceba6e7e89a5257b3c318819850da51&language=en-US&page=1`;
 
       const res = await fetch(reviewsURL);
       const data = await res.json();
 
-      // console.log(data.results);
-      showReviewsInDOM(data.results);
+      const movie = movies.filter((movie) => movie.id === +id);
+
+      showReviewsInDOM(data.results, movie[0]);
     });
   });
 }
 
 // Showing reviews in DOM
-function showReviewsInDOM(reviews) {
+function showReviewsInDOM(reviews, movie) {
+  hero.classList.add("hide");
+  const div = document.createElement("div");
+  div.classList.add("movie-details");
+  const poster = `${posterURL}${movie.poster_path}`;
+
+  let data = `
+    <aside>
+      <img src="${poster}" alt="${movie.title}"/>
+    </aside>
+    <section class="details-section">
+      <h2>${movie.title}</h2>
+      <p><strong>Releasing:</strong> ${movie.release_date}</p>
+      <p><strong>ImDB Rating:</strong> ${movie.vote_average}</p>
+      <p><strong>Overview:</strong> ${movie.overview}</p>
+    </section>
+    <main class="all-reviews">
+
+  `;
+
   reviews.forEach((review) => {
-    console.log(review);
+    let avatar = review.author_details.avatar_path;
+    const prefix = "https://secure.gravatar.com/avatar/";
+    const createdAt = new Date(review.created_at);
+    const date = createdAt.getDate();
+    const month = createdAt.getMonth();
+    const year = createdAt.getFullYear();
+    const hour = createdAt.getHours();
+    const minute = createdAt.getMinutes();
+
+    if (avatar !== null && avatar.slice(1).lastIndexOf("https") === 0) {
+      avatar = avatar.slice(1);
+    } else if (avatar !== null) {
+      avatar = prefix + avatar.slice(1);
+    }
+
+    data += `
+    <div class="review">
+      <div class="person-details">
+        <h2>Username: ${review.author}</h2>
+        <img src="${avatar !== null ? avatar : ""}" alt="${review.author}"/>
+        <span>Commented: ${date}-${month}-${year} || ${hour}:${minute}</span>
+      </div>
+
+      <p>${review.content}</p>
+    </div>
+    `;
   });
+
+  data += `</main>`;
+  div.innerHTML = data;
+  reviewsDiv.appendChild(div);
 }
